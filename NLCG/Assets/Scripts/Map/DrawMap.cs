0e4bcf41370541e45c85b3c7b2f4ea7c272a52bd
daonq2001 <daonq.nde19046@vtc.edu.vn>
2020-08-30 06:45:05 +0700

    Ve map nhung chua dung y cua minh

--- NLCG/Assets/Scripts/Map/DrawMap.cs
+++ NLCG/Assets/Scripts/Map/DrawMap.cs
@@ -1,39 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class DrawMap : MonoBehaviour
 {
     public int xSize, ySize;
     public GameObject tile;
-
+    public List<Sprite> sprites;
+    
     private GameObject[,] tiles;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
         CreateBoard(offset.x, offset.y);
     }
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     private void CreateBoard(float xOffset, float yOffset)
     {
-        tiles = new GameObject[xSize, ySize];     // 9
+        tiles = new GameObject[xSize, ySize];     
 
-        float startX = transform.position.x;     // 10
+        float startX = transform.position.x;     
         float startY = transform.position.y;
 
         for (int x = 0; x < xSize; x++)
-        {      // 11
+        {      
             for (int y = 0; y < ySize; y++)
             {
+                if(y == 0 || x == 0 || y == xSize - 1 || x == ySize - 1)
+                {
+                    tile.GetComponent<SpriteRenderer>().sprite = sprites[0];
+                }
+                else
+                {
+                    tile.GetComponent<SpriteRenderer>().sprite = sprites[1];
+                }
                 GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
-                tiles[x, y] = newTile;
+                
             }
         }
     }
---
+++ NLCG/Assets/Scripts/Map/TestMouse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMouse : MonoBehaviour
+{
+    private Vector3 mouse;
+
+    private void OnMouseDrag()
+    {
+        mouse = Input.mousePosition;
+        mouse = Camera.main.ScreenToWorldPoint(mouse);
+        mouse.z = 0;
+        transform.position = mouse;
+        transform.position = new Vector2(Mathf.RoundToInt(mouse.x), Mathf.RoundToInt(mouse.y));
+    }
+}
